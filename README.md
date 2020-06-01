98165eb7d3c73f2435c854b1bb185b93b5303505
gelissa <gelissaleveille@yahoo.com>
2020-06-01 02:56:47 -0400

    added updated protest map

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ An aggregated guide with resources for those who are resisting violence
 ![No Justice, No Peace](assets/nj_np.png)
 
 https://gelissa.github.io/aGuide/
+
+## Thanks also to:
+
+![MediaInfo.js](https://mediainfo.js.org/#mediainfojs)
+![Simple MetaData Extractor](https://codepen.io/buzzone/pen/eYNjJrx)
---
+++ script.js/scrubber.js
@@ -0,0 +1,36 @@
+const fileinput = document.getElementById('fileinput')
+const output = document.getElementById('output')
+
+const onChangeFile = (mediainfo) => {
+  const file = fileinput.files[0]
+  if (file) {
+    output.value = 'Working…'
+
+    const getSize = () => file.size
+
+    const readChunk = (chunkSize, offset) =>
+      new Promise((resolve, reject) => {
+        const reader = new FileReader()
+        reader.onload = (event) => {
+          if (event.target.error) {
+            reject(event.target.error)
+          }
+          resolve(new Uint8Array(event.target.result))
+        }
+        reader.readAsArrayBuffer(file.slice(offset, offset + chunkSize))
+      })
+
+    mediainfo
+      .analyzeData(getSize, readChunk)
+      .then((result) => {
+        output.value = result
+      })
+      .catch((error) => {
+        output.value = `An error occured:\n${error.stack}`
+      })
+  }
+}
+
+MediaInfo({ format: 'text' }, (mediainfo) => {
+  fileinput.addEventListener('change', () => onChangeFile(mediainfo))
+})
